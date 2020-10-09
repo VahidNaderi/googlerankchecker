6b26e9a32077a38d04c40bd8e75f0b6d2c071475
v.naderi <v.naderi@arsina.net>
2020-10-09 22:07:32 +0330

    Add releases folder creation on deployment

--- deploy.js
+++ deploy.js
@@ -8,20 +8,29 @@ var buildLocation = path.join(__dirname, "releases");
 let appFolder = path.join(__dirname, "src");
 let manifestFilePath = path.join(appFolder, "manifest.json");
 
-// read manifest file
-var manifest = jsonfile.readFileSync(manifestFilePath);
-function getNewVersion() {
-        let splitedVer =manifest.version.split('.');
-       var ver = parseInt(splitedVer[2]);
-       ver++;
-       return `${splitedVer[0]}.${splitedVer[1]}.${ver}`;
-}
-var version = getNewVersion();
-// replace version
-manifest.version = version;
-// save manifest file
-jsonfile.writeFileSync(manifestFilePath, manifest);
+fs.exists(buildLocation).then(res => {
+        if (!res) {
+                fs.mkdir(buildLocation);
+        }
+        preparePackage();
+});
 
-// create zip
-zipper.sync.zip(appFolder).compress().save(path.join(buildLocation, "googlerankchecker.zip"));
-//const fileBin = fs.readFileSync(path.join(buildLocation, "googlerankchecker.zip"));
+function preparePackage() {
+        // read manifest file
+        var manifest = jsonfile.readFileSync(manifestFilePath);
+        function getNewVersion() {
+                let splitedVer = manifest.version.split('.');
+                var ver = parseInt(splitedVer[2]);
+                ver++;
+                return `${splitedVer[0]}.${splitedVer[1]}.${ver}`;
+        }
+        var version = getNewVersion();
+        // replace version
+        manifest.version = version;
+        // save manifest file
+        jsonfile.writeFileSync(manifestFilePath, manifest);
+
+        // create zip
+        zipper.sync.zip(appFolder).compress().save(path.join(buildLocation, "googlerankchecker.zip"));
+        //const fileBin = fs.readFileSync(path.join(buildLocation, "googlerankchecker.zip"));
+}
